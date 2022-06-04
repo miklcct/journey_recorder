5a50d63725a1705a19893d88e9576d3b6fc3c057
Michael Tsang <miklcct@gmail.com>
2022-06-04 23:41:42 +0100

    use the whole price as the price used for expired ticktes

---
+++ resource/migrations/up/00013.sql
@@ -0,0 +1,52 @@
+-- Migrate to Version 13 
+alter view `tickets view` as
+    select serial
+        , description
+        , advance
+        , `maximum group size`
+        , currency
+        , price
+        , carnets
+        , expired
+        , `first use`
+        , `last use`
+        , `carnets used`
+        , `segments travelled`
+        , `distance travelled`
+        , if(expired, price, price / carnets * `carnets used`) as `price used`
+        , if(expired, price, price / carnets * `carnets used`) / `distance travelled` as `price per km`
+    from tickets join (
+        select serial as `_serial`
+        , (
+            select max(`group size`)
+            from `ticket uses` join journeys on `journey serial` = serial
+            where `ticket serial` = tickets.serial
+        ) as `maximum group size`
+        , (
+            select min(`boarding time stamp`)
+            from `ticket uses` join journeys on `journey serial` = serial
+            where `ticket serial` = tickets.serial
+        ) as `first use`
+        , (
+            select max(`alighting time stamp`)
+            from `ticket uses` join journeys on `journey serial` = serial
+            where `ticket serial` = tickets.serial
+        ) as `last use`
+        , (
+            select count(distinct `carnet sequence`)
+            from `ticket uses`
+            where `ticket serial` = tickets.serial
+        ) as `carnets used`
+        , (
+            select count(distinct `journey serial`)
+            from `ticket uses`
+            where `ticket serial` = tickets.serial
+        ) as `segments travelled`
+        , (
+            select ifnull(sum(ifnull(`distance covered`, distance) * `group size`),0)
+            from `ticket uses` join journeys on `journey serial` = serial
+            where `ticket serial` = tickets.serial
+        ) as `distance travelled`
+        from tickets
+    ) as `tickets info` on tickets.serial = `tickets info`._serial
+    order by `tickets info`.`last use` desc
