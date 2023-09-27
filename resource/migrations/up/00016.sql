fd698db70d9249a40c8e2f02e9b9121820b7e0d4
Michael Tsang <miklcct@gmail.com>
2023-09-27 15:54:14 +0100

    fix SQL

--- resource/migrations/up/00016.sql
+++ resource/migrations/up/00016.sql
@@ -1,2 +1,2 @@
 alter table journeys
-    modify type enum('Aeroplane','Helicopter','Train','Metro','Tram','Funicular','Coach','BRT','Bus','Trolleybus','Share taxi','Ferry','Cable Car') default null
+    modify type enum('Aeroplane','Helicopter','Train','Metro','Tram','Funicular','Coach','BRT','Bus','Trolleybus','Share taxi','Ferry','Cable Car') not null
